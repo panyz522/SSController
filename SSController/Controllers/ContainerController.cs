58926f840aadbf70c88493a2156f2fd92ba15cdf
Yongzhen Pan <panyz522@gmail.com>
2019-08-09 10:25:36 +0800

    Add project

---
+++ SSController/Controllers/ContainerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContainerController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "1.2.3.4";
+        }
+    }
+}
