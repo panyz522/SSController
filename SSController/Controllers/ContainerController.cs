a1e30af1efb39d0ab1963eeba15932c616c62adc
Yongzhen Pan <panyz522@gmail.com>
2019-08-09 17:50:25 +0800

    Add restart command

--- SSController/Controllers/ContainerController.cs
+++ SSController/Controllers/ContainerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Management.ContainerInstance.Fluent;
 using Microsoft.Azure.Management.Fluent;
 using SSController.Services;
 using SSController.Utils;
@@ -23,14 +24,8 @@ namespace SSController.Controllers
         [HttpGet]
         public async Task<ActionResult<string>> GetAsync()
         {
-            // Get Azure auth file for login.
-            var client = this.blobClientProvider.Create("personal0");
-            string authFilePath = Path.Combine(IOUtils.GetDataFolderPath(), ".azureauth");
-            await client.DownloadAllTextAsync("secret/azure.azureauth", authFilePath);
-
             // Create IAzure and check the container.
-            var azure = Azure.Authenticate(authFilePath).WithDefaultSubscription();
-            var container = azure.ContainerGroups.ListByResourceGroup("gp-japaneast-ctnr").FirstOrDefault();
+            var container = await GetContainerAsync();
             if (container == null)
             {
                 return this.BadRequest("No container found.");
@@ -41,15 +36,44 @@ namespace SSController.Controllers
 
         // Set ip
         [HttpPost]
-        public ActionResult Post([FromQuery] string ip)
+        public async Task<ActionResult<string>> Post([FromQuery] string command)
         {
-            System.IO.File.WriteAllText(this.GetIpFilePath(), ip);
-            return this.Ok();
+            // Create IAzure and check the container.
+            var container = await this.GetContainerAsync();
+            if (container == null)
+            {
+                return this.BadRequest("No container found.");
+            }
+
+            string res;
+            switch (command)
+            {
+                case "restart":
+                    await container.RestartAsync();
+                    res = "Success";
+                    break;
+                default:
+                    res = "No command";
+                    break;
+            }
+
+            return res;
         }
 
         private string GetIpFilePath()
         {
             return Path.Combine(IOUtils.GetDataFolderPath(), "ip.txt");
+        }
+
+        private async Task<IContainerGroup> GetContainerAsync()
+        {
+            // Get Azure auth file for login.
+            var client = this.blobClientProvider.Create("personal0");
+            string authFilePath = Path.Combine(IOUtils.GetDataFolderPath(), ".azureauth");
+            await client.DownloadAllTextAsync("secret/azure.azureauth", authFilePath);
+
+            var azure = Azure.Authenticate(authFilePath).WithDefaultSubscription();
+            return azure.ContainerGroups.ListByResourceGroup("gp-japaneast-ctnr").FirstOrDefault();
         }
     }
 }
